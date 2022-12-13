cb67e9d4ea8ea4a777099564092a9dce8ca955c5
AmanGiri007 <giriaman437@gmail.com>
2022-12-13 14:08:30 +0545

    tags haleko

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,12 @@ from . import views
 urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
     path('article-list', views.UserListView.as_view(), name="article-list"),
+    path('article-list/national', views.NationalView.as_view(), name="national"),
+    path('article-list/fashion', views.FashionView.as_view(), name='fashion'),
+    path('article-list/sports', views.SportsView.as_view(), name='sports'),
+    path('article-list/politics', views.PoliticsView.as_view(), name='politics'),
+    path('article-list/others', views.OthersView.as_view(), name='others'),
+    path('article-list/finance', views.FinanceView.as_view(), name='finance'),
     path('article-create', views.articleCreate, name="article-create"),
     path('article-detail/<str:pk>/', views.articleDetail, name="article-detail"),
     path('article-update/<str:pk>/', views.articleUpdate, name="article-update"),
--- api/views.py
+++ api/views.py
@@ -23,17 +23,49 @@ def apiOverview(request):
     return Response(api_urls)
 
 
-# @api_view(['GET'])
-# def articleList(request):
-#     articles = Article.objects.all()
-#     print(articles)
-#     serializer = ArticleSerializer(articles, many=True)
-#     return Response(serializer.data)
+@api_view(['GET'])
+def articleList(request):
+    articles = Article.objects.all()
+    print(articles)
+    serializer = ArticleSerializer(articles, many=True)
+    return Response(serializer.data)
+
+
 class UserListView(generics.ListAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-    filter_backends = [SearchFilter]
-    search_fields = ['=headline']
+    # filter_backends = [SearchFilter]
+    # search_fields = ['=headline']
+
+
+class NationalView(generics.ListAPIView):
+    queryset = Article.objects.filter(tags='NATIONAL')
+    serializer_class = ArticleSerializer
+
+
+class FashionView(generics.ListAPIView):
+    queryset = Article.objects.filter(tags='FASHION')
+    serializer_class = ArticleSerializer
+
+
+class SportsView(generics.ListAPIView):
+    queryset = Article.objects.filter(tags='SPORTS')
+    serializer_class = ArticleSerializer
+
+
+class PoliticsView(generics.ListAPIView):
+    queryset = Article.objects.filter(tags='POLITICS')
+    serializer_class = ArticleSerializer
+
+
+class FinanceView(generics.ListAPIView):
+    queryset = Article.objects.filter(tags='FINANCE')
+    serializer_class = ArticleSerializer
+
+
+class OthersView(generics.ListAPIView):
+    queryset = Article.objects.filter(tags='OTHERS')
+    serializer_class = ArticleSerializer
 
 
 @api_view(['POST'])
