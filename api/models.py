1d165d025175a3cbb18d1265e9f9de48b5158462
Anuj-Gupta4 <anujgupta.4388@gmail.com>
2022-12-14 16:50:31 +0545

    updated views to display only published articles

--- api/models.py
+++ api/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-
 class Article(models.Model):
     tag_choice = [
         ('NATIONAL', 'National'),
--- api/views.py
+++ api/views.py
@@ -1,14 +1,11 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import ArticleSerializer
 from .models import Article
 from rest_framework.filters import SearchFilter
 from rest_framework import generics
+
 # Create your views here.
-
-
 @api_view(['GET'])
 def apiOverview(request):
     api_urls = {
@@ -28,55 +25,54 @@ def apiOverview(request):
     return Response(api_urls)
 
 
-@api_view(['GET'])
-def articleList(request):
-    articles = Article.objects.all()
-    print(articles)
-    serializer = ArticleSerializer(articles, many=True)
-    return Response(serializer.data)
-
-
-class UserSearchView(generics.ListAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-    filter_backends = [SearchFilter]
-    search_fields = ['$tags', '=id', '$headline', '$byline']
+# @api_view(['GET'])
+# def articleList(request):
+#     articles = Article.objects.filter(published=True)
+#     print(articles)
+#     serializer = ArticleSerializer(articles, many=True)
+#     return Response(serializer.data)
 
 
 class UserListView(generics.ListAPIView):
-    queryset = Article.objects.all()
+    # queryset = Article.objects.all()
+    queryset = Article.objects.filter(published=True)
     serializer_class = ArticleSerializer
-    # filter_backends = [SearchFilter]
-    # search_fields = ['=headline']
+
+
+class UserSearchView(generics.ListAPIView):
+    queryset = Article.objects.filter(published=True)
+    serializer_class = ArticleSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['$tags', '=id', '$headline', '$byline', '$published']
 
 
 class NationalView(generics.ListAPIView):
-    queryset = Article.objects.filter(tags='NATIONAL')
+    queryset = Article.objects.filter(tags='NATIONAL', published = True)
     serializer_class = ArticleSerializer
 
 
 class FashionView(generics.ListAPIView):
-    queryset = Article.objects.filter(tags='FASHION')
+    queryset = Article.objects.filter(tags='FASHION', published = True)
     serializer_class = ArticleSerializer
 
 
 class SportsView(generics.ListAPIView):
-    queryset = Article.objects.filter(tags='SPORTS')
+    queryset = Article.objects.filter(tags='SPORTS', published = True)
     serializer_class = ArticleSerializer
 
 
 class PoliticsView(generics.ListAPIView):
-    queryset = Article.objects.filter(tags='POLITICS')
+    queryset = Article.objects.filter(tags='POLITICS', published = True)
     serializer_class = ArticleSerializer
 
 
 class FinanceView(generics.ListAPIView):
-    queryset = Article.objects.filter(tags='FINANCE')
+    queryset = Article.objects.filter(tags='FINANCE', published = True)
     serializer_class = ArticleSerializer
 
 
 class OthersView(generics.ListAPIView):
-    queryset = Article.objects.filter(tags='OTHERS')
+    queryset = Article.objects.filter(tags='OTHERS', published = True)
     serializer_class = ArticleSerializer
 
 
